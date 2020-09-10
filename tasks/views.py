d7a40190e6fcf36200581af1e82b73367b8312eb
milotr <minhlong010122@gmail.com>
2020-09-10 16:10:22 +0300

    New django study files
    Client-Server validation
    Forms
    Sessions
    Conditional logic
    Specific linking

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,46 @@
+from django import forms
 from django.shortcuts import render
-tasks = ["foo", "bar", "baz"]
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 # These function will request the HTML pages. 
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value= 1, max_value= 10)
+
+
 def index(request):
+    #creating sessions
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
         
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    #Client-Server validation:
+    #if the user submit a form
+    if request.method == "POST":
+        #then get all the data into form
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            # if Form is valid, get cleaned data from "task" into variable 
+            # task and add it into the tasks list.
+            task = form.cleaned_data["task"]
+            task.append(task)
+            #?
+            request.session["task"] += [task]
+            #Redirect after added the new string to the list
+            #reverse("tasks:index") is django will figure out the 
+            #name "index" and return the html page
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html",{
+        "form": NewTaskForm()
+    })
